a610f69b69f2ddbe0bf15ce10cdd4e72a3482572
Moises <liricractos@gmail.com>
2021-01-14 20:38:52 +0000

    Criei a class funcinário
    Serias dificuldades em criar os metodos. Diogo Ve.

---
+++ SuperMercado/SuperMercado/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperMercado
+{
+    class Funcionario
+    {
+        //Atributos
+        string nome;
+
+        //Construtor
+        public Funcionario(string nome)
+        {
+            this.nome = nome;
+        }
+
+        //GET e SET 
+        public string Nome { get => nome; set => nome = value; }
+
+        //METODOS
+        //Por favor, Diogo faz tu xD
+    }
+}
