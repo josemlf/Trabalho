e0f9912e43b7dcfdb1c4955448bdc2495bd30590
Macedo <diogomacedo2801@gmail.com>
2021-01-14 21:54:03 +0000

    Login
    em progesso

---
+++ SuperMercado/SuperMercado/LoginGes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperMercado
+{
+    class LoginGes
+    {
+        private string nome;
+        private int id;
+        private string pass;
+
+        public LoginGes(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public 
+
+    }
+}
